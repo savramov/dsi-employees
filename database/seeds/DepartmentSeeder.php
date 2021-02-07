2d7daf7f4b2534f6d7715bee8565ea89f929bcbf
Svetlomir Avramov <s_avramov@abv.bg>
2021-02-07 12:33:16 +0200

    departments table seeder - done;

---
+++ database/seeds/DepartmentSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DepartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $departments = [
+            ['id' => 1, 'name' => 'Sales & Marketing', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+            ['id' => 2, 'name' => 'Customer Support', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+            ['id' => 3, 'name' => 'Human Resources', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+            ['id' => 4, 'name' => 'IT', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')]
+        ];
+
+        DB::table('departments')->insert($departments);
+    }
+}
